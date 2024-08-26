52d1ef7894613223bb2198c41cf9cd07283e0e0c
zanguim <leonelzanguim4@gmail.com>
2024-08-26 16:27:09 +0000

    add input,property,dynamic value and ngfor in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./home-page/home-page.component";
+import { ImageModule } from 'primeng/image';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent],
+  imports: [RouterOutlet, HomePageComponent,ImageModule,CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/housinglocation/housinglocation.component.ts
+++ src/app/housinglocation/housinglocation.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Housinglocation } from './../housinglocation';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-housinglocation',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './housinglocation.component.html',
   styleUrl: './housinglocation.component.scss'
 })
 export class HousinglocationComponent {
+
+  @Input() housingLocation!: Housinglocation;
 
 }
