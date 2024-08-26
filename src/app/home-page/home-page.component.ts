646879f8da036b10c9611a94bbc6d4cf2a4830a8
zanguim <leonelzanguim4@gmail.com>
2024-08-26 12:48:28 +0000

    create new interface

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
+import { HousinglocationComponent } from '../housinglocation/housinglocation.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    HousinglocationComponent
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
---
+++ src/app/housinglocation/housinglocation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housinglocation',
+  standalone: true,
+  imports: [],
+  templateUrl: './housinglocation.component.html',
+  styleUrl: './housinglocation.component.scss'
+})
+export class HousinglocationComponent {
+
+}
